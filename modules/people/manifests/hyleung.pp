d4bc2c61924bf7e689271a498e3a9c2d40b386b3
H.Y. Leung <hy.leung@gmail.com>
2013-03-21 22:33:04 -0700

    Added personal manifest

---
+++ modules/people/manifests/hyleung.pp
@@ -0,0 +1,11 @@
+class people::hyleung {
+    package {
+        'android-sdk':
+            provider => 'homebrew',
+            ensure => 'r21.1';
+        'android-ndk':
+            provider => 'homebrew',
+            ensure => 'r8d';
+    }
+
+}
